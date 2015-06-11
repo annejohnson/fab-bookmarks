608e6c0ba5982c632f7064f700bfe98203b1175a
Anne <annekjohnson23@yahoo.com>
2015-06-11 07:54:40 -0400

    Pretty error notification for the bookmark form

--- js/bookmarks.js
+++ js/bookmarks.js
@@ -85,10 +85,26 @@ var LoginForm = React.createClass({
   }
 });
 var BookmarkForm = React.createClass({
+  getInitialState: function() {
+    return { error: null };
+  },
   componentDidMount: function() {
     this.refs.title.getDOMNode().value = '';
     this.refs.categories.getDOMNode().value = '';
     this.refs.url.getDOMNode().value = 'http://';
+  },
+  validUrl: function(url) {
+    return url.match(/https?:\/\/(?:www\.|(?!www))[^\s\.]+\.[^\s]{2,}|www\.[^\s]+\.[^\s]{2,}/);
+  },
+  hasErrorState: function(title, url) {
+    if (!title || !this.validUrl(url)) {
+      this.setState({
+        error: (!title ? "Title must not be blank" : "URL is invalid")
+      });
+      return true;
+    }
+    this.setState({ error: null });
+    return false;
   },
   handleSubmit: function(e) {
     e.preventDefault();
@@ -97,7 +113,7 @@ var BookmarkForm = React.createClass({
     var categories = this.refs.categories.getDOMNode().value.split(",").map(function(category) {
       return category.trim();
     }).filter(function(category) { return category; });
-    if (!title || !url)
+    if (this.hasErrorState(title, url))
       return;
     bookmarksDatabase.push({ title: title, url: url, categories: categories });
     this.refs.title.getDOMNode().value = '';
@@ -105,9 +121,13 @@ var BookmarkForm = React.createClass({
     this.refs.url.getDOMNode().value = 'http://';
   },
   render: function() {
+    var errorNotification = this.state.error ? (
+      <div className="alert-box alert" style={formErrorStyles}>{this.state.error}</div>
+    ) : "";
     return (
       <form className="bookmarkForm panel large-10 large-centered columns" onSubmit={this.handleSubmit} style={formStyles}>
         <h5 className="text-center" style={formHeaderStyles}>Add a New Bookmark:</h5>
+        {errorNotification}
         <div className="row">
           <div className="small-6 columns">
             <input type="text" placeholder="Title" ref="title" />
